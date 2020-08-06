7b8153a330c6d172bc1de571ae4cbc7ab3cba320
Tim Adair <tymmo.t@gmail.com>
2020-08-06 15:47:01 -0600

    Add H2 database and table with default entry

--- server-demo/src/main/java/com/timadair/serverdemo/controllers/EntryController.java
+++ server-demo/src/main/java/com/timadair/serverdemo/controllers/EntryController.java
@@ -1,6 +1,8 @@
 package com.timadair.serverdemo.controllers;
 
+import com.timadair.serverdemo.repositories.EntryDao;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "Entry Controller", produces = MediaType.APPLICATION_JSON_VALUE)
 public class EntryController {
 
-  @GetMapping(path = "/entries")
+  private EntryDao entryDao;
+
+  @Autowired
+  public EntryController(EntryDao entryDao) {
+    this.entryDao = entryDao;
+  }
+
+  @GetMapping(path = "/entries/{id}")
   public String getEntry(@RequestParam int id) {
-    return "test";
+    return entryDao.getById(id);
   }
 }
---
+++ server-demo/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE entries (id INT AUTO_INCREMENT PRIMARY KEY,
+                      entry_text TEXT NOT NULL
+);
+
+INSERT INTO entries (entry_text) VALUES
+    ('Default Entry One');
