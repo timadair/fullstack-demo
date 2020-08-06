b8dd015a1fc80abcbb687d94a82fd1a3ef031052
Tim Adair <tymmo.t@gmail.com>
2020-08-06 16:23:38 -0600

    Render List of entries

--- react-ui-demo/src/list/Entries.jsx
+++ react-ui-demo/src/list/Entries.jsx
@@ -1,9 +1,21 @@
 import * as React from "react";
 
 export class Entries extends React.Component {
+
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            entries: [
+              'First Entry',
+              'Second Entry'
+            ],
+        }
+    }
+
     render() {
-        return <p>
-            Test Entries Text
-        </p>;
+        return <ol>
+            {this.state.entries.map(text => <li>{text}</li>)}
+        </ol>;
     }
 }
