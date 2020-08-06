0dbc92b7bd5ed96fc750df7c3f1d9f8c4708ba55
Tim Adair <tymmo.t@gmail.com>
2020-08-06 14:38:58 -0600

    Add generated SpringBoot project

---
+++ server-demo/src/test/java/com/timadair/serverdemo/ServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timadair.serverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
