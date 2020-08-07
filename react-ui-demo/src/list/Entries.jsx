44dffbac3cb90312d0ab8ea630d018e5b22ba813
Tim Adair <tymmo.t@gmail.com>
2020-08-07 01:34:37 -0600

    Add Material UI TextField Input, not yet POSTing

--- react-ui-demo/src/list/Entries.jsx
+++ react-ui-demo/src/list/Entries.jsx
@@ -1,8 +1,12 @@
 import * as React from "react";
+import TextField from "@material-ui/core/TextField";
 
 export class Entries extends React.Component {
+
   constructor(props, context) {
     super(props, context);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
       entries: [
         {
@@ -10,6 +14,7 @@ export class Entries extends React.Component {
           entryText: 'Example Entry'
         },
       ],
+      latestInputText: ""
     }
   }
 
@@ -32,6 +37,37 @@ export class Entries extends React.Component {
   }
 
   render() {
+    return <div>
+      {this.renderInputForm()}
+      {this.renderList()}
+    </div>;
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.latestInputText);
+    this.setState({
+      latestInputText: ""
+    });
+  }
+
+  handleChange(event) {
+    this.setState({
+      latestInputText: event.target.value
+    })
+  }
+
+  renderInputForm() {
+    return <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
+      <TextField id="new-entry-inputs"
+                 label="New List Entries"
+                 variant="filled"
+                 value={this.state.latestInputText}
+                 onChange={this.handleChange}/>
+    </form>
+  }
+
+  renderList() {
     return <ol>
       {
         this.state.entries.map(entry =>
